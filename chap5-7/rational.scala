bedceb94afb6a398a72a09fd48d6c1a195eceb07
sythryan <syth.ryan@banno.com>
2014-05-05 14:06:18 -0500

    added a def add

--- chap5-7/rational.scala
+++ chap5-7/rational.scala
@@ -1,4 +1,12 @@
 class Rational (n: Int, d: Int) {
+  val numer :Int = n
+  val denom :Int = d
   require (d != 0)
-  override def toString = n + "/" + d
+  override def toString = numer + "/" + denom
+  def add(that: Rational): Rational = {
+    new Rational (
+      numer * that.denom + denom * that.numer,
+      denom * that.denom
+    )
+  }
 }
