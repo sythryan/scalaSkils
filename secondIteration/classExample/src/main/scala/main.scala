11972e7b590942a64ad01f0aa7e7d7c5069c5e28
sythryan <syth.ryan@banno.com>
2014-05-12 14:16:17 -0500

    version without using factory

--- secondIteration/classExample/src/main/scala/main.scala
+++ secondIteration/classExample/src/main/scala/main.scala
@@ -3,10 +3,15 @@ package moneyExample
 object main {
   def main(args: Array[String]): Unit = {
     val factory = Currency
-    val myCoin = factory.coin(0.25)
-    // println ("Name:   " + myCoin.name)
+    val myCoin = new Coin(0.25)
+    println ("Name:   " + myCoin.name)
     println ("Value:  " + myCoin.value)
-    val myBill = factory.dollar (20)
+    val myBill = new Dollar (20)
     println ("Value:  " + myBill.value)
+    println (myBill.value + " breaks into " + myBill.break(5) + " fives")
+    println (myBill.value + " breaks into " + myBill.break(1) + " ones")
+    println (myBill.value + " breaks into " + myBill.break(10) + " tens")
+    println (myBill.value + " breaks into " + myBill.break(20) + " twenties")
+    println (myBill.value + " breaks into " + myBill.break(50) + " fifties")
   }
 }
--- secondIteration/currency.scala
+++ secondIteration/currency.scala
@@ -3,3 +3,13 @@ package moneyExample
 abstract class Currency {
   def value : Double
 }
+
+object Currency {
+
+  def dollar (bill: Double): Currency =
+    new Dollar (bill)
+
+  def coin (coinValue: Double): Currency =
+    new Coin (coinValue)
+  
+}
