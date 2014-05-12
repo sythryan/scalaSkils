58c94e7676b5365bbe928ebdf07af698a9db9d07
sythryan <syth.ryan@banno.com>
2014-05-12 10:41:53 -0500

    implemented class functions to make them none abstract

--- secondIteration/coin.scala
+++ secondIteration/coin.scala
@@ -2,5 +2,12 @@ package moneyExample
 
 class Coin (coinValue : Double) extends Currency {
   def value : Double = coinValue
-  def amountInDollar : Int  // returns how many coins it takes to make a dollar, example: quater would return 4
+  def name : String = coinValue match {
+    case 0.50 => "Half Dollar"
+    case 0.25 => "Quater"
+    case 0.10 => "Dime"
+    case 0.05 => "Nickel"
+    case 0.01 => "Penny"
+    case _    => "Unknown" 
+  }
 }
--- secondIteration/dollar.scala
+++ secondIteration/dollar.scala
@@ -1,6 +1,12 @@
 package moneyExample
 
 class Dollar (bill: Double) extends Currency {
-  def value : Double = Bill
-  def Break (breakIntoBills : Int): Int  // returns, for example if bill was 5 and Break (1) sould return 5
+  def value : Double = bill
+  def Break (breakIntoBills : Int): Int = {
+    if (breakIntoBills > bill.toInt) {
+      0
+    } else {
+      bill.toInt / breakIntoBills
+    }
+  }
 }
