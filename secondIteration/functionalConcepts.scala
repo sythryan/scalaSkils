967ff893bfe155da2b761c85bd79ade72a932696
sythryan <syth.ryan@banno.com>
2014-05-12 09:10:34 -0500

    tail recursive conversion

--- secondIteration/functionalConcepts.scala
+++ secondIteration/functionalConcepts.scala
@@ -3,13 +3,24 @@ object FunctionalConcepts {
   def add(x:Int)(y:Int) = x + y
 
   def increment (x: Int): Int = add(x)(1)
-  
+
   def decrement (x: Int): Int = add(x)(-1)
 
   def mapOverList(ints: List[Int], f: Int => Int): List[Int] = {
-    ints match {
-      case Nil => ints
-      case head :: tail => f(head) :: mapOverList(tail, f)
+    def innerMapOverList(ints: List[Int], f: Int => Int): List[Int] = {
+      ints match {
+        case Nil => ints
+        case head :: tail => f(head) :: innerMapOverList(tail, f)
+      }
     }
+    innerMapOverList (ints, f)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val myList = List (1, 2, 3, 5, 3, 7)
+    val incrementList = mapOverList(myList, increment)
+    println ("Increment list: " + incrementList)
+    val decrementList = mapOverList(myList, decrement)
+    println ("Decrement list " + decrementList)
   }
 }
