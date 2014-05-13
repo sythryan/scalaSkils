2126268a7147568bdeca41cef05105ff46d43d06
sythryan <syth.ryan@banno.com>
2014-05-13 13:36:22 -0500

    middle of milestone

--- thirdIteration/milestone/src/main/scala/milestoneProject.scala
+++ thirdIteration/milestone/src/main/scala/milestoneProject.scala
@@ -2,21 +2,56 @@ object milestoneProject {
 
   case class User(id: String, name: String, balance: Double, transactions: List[Transaction])
   case class Transaction(id: String, amount: Double)
-
-  var memoryBasedUserRepository = scala.collection.mutable.Map[String,User]()
   
   trait UserRepository {
     def getAll: List[User]
-    def create(x: User): User = {
-      memoryBasedUserRepository(x.id) = x
-    }
-    def update(x: User): User = {
-      memoryBasedUserRepository(x.id) = x
-    }
+    def create(x: User): User
+    def update(x: User): User
     def delete(x: User): User
   }
   
-  // Transaction case class, containing an amount and an ID unique to that user). You will then need to create an implementation, 
-  // MemoryBasedUserRepository, that stores all users in a data structure that your program holds locally. Think about what kind 
-  // of data structure to use so your performance will not suffer if you get a large number of users!
+  trait memoryBasedUserRepository  extends UserRepository {
+    var userStorage = scala.collection.mutable.Map[String,User]()
+    def getAll: List[User] = {
+      userStorage.values.toList  
+    }
+    def create(x: User): User = {
+      userStorage(x.id) = x
+      x
+    }
+    def update(x: User): User = {
+      userStorage(x.id) = x
+      x
+    }
+    def delete(x: User): User = {
+      userStorage -= x.id
+      x
+    }
+  }
+
+  class userManipulator extends memoryBasedUserRepository {
+
+    def findUser(theID: String): Option[User] = theID {
+      def recursiveFindUser(userList: List[User]): Option[User] = userList match {
+        case Nil => None
+        case head :: tail => 
+          head.id match {
+            case a if (a == theID) => some(head)
+            case _ => recursiveFindUser(tail)
+          }
+      } 
+      recursiveFindUser(getAll)     
+    }
+
+    // def findAllUsersAbove (balance: Double, users: List[User]): List[User] = {
+    //   def recursiveFindallUsersAbove(userList: List[User]): List[User] = userList.balance match {
+    //     case _ => 
+    //   }
+    // }
+  }
+  
+// Find a single user by ID.
+// Find all users above a given balance
+// Find the value of the bank (that is, the sum of all balances from all users)
+// Add a list of transactions to a single user.
 }
