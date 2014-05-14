a39ec3719aea5489a78c1c4936345e777d8de312
sythryan <syth.ryan@banno.com>
2014-05-14 16:16:16 -0500

    middle of copress

--- fourthIteration/99Problems/src/main/scala/main.scala
+++ fourthIteration/99Problems/src/main/scala/main.scala
@@ -1,6 +1,6 @@
 object main {
 
-  def last[A](theList: List[A]): A ={
+  def last[A](theList: List[A]): A = {
     theList.reverse.head // or theList.last
   }
 
@@ -10,15 +10,15 @@ object main {
     case _ => throw new NoSuchElementException
   }
 
-  def nth[A](n: Int, theList: List[A]): A ={
+  def nth[A](n: Int, theList: List[A]): A = {
     theList(n)
   }
 
-  def length[A](theList: List[A]): Int ={
+  def length[A](theList: List[A]): Int = {
     theList.length
   }
 
-  def reverse[A](theList: List[A]): List[A] ={
+  def reverse[A](theList: List[A]): List[A] = {
     theList.reverse
   }
 
@@ -31,10 +31,19 @@ object main {
     case a => List(a) 
   }
 
+  def compress[A](theList: List[A]): List[A] = theList match {
+    case a :: b :: tail if (a == b) => List(a) ::: compress(tail)
+    case a :: b :: Nil  if (a == b) => List(a)
+    case a :: Nil => List(a)
+  }
+
+
+
   def main(args: Array[String]): Unit = {
     val testList = List (1, 1, 2, 3, 5, 8)
     val palindromeList = List (1, 2, 3, 2, 1)
     val nestedList = List (List(1, 1), 2, List(3, List(5, 8)))
+    val symbolList = List ('a, 'a, 'a, 'a, 'b, 'c, 'c,'a, 'a, 'd, 'e, 'e, 'e, 'e)
     println("Last result       : " + last(testList))
     println("penultimate result: " + penultimate(testList))
     println("kth element result: " + nth(2, testList))
@@ -43,5 +52,6 @@ object main {
     println("not palindrome    : " + isPalindrome(testList))
     println("is a palindrome   : " + isPalindrome(palindromeList))
     println("flatten  result   : " + flatten(nestedList))
+    println("compress result   : " + compress(symbolList))
   }  
 }
