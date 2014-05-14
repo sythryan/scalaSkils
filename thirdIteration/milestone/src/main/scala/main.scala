124beb1de4087627016447c5daa66639853fbf4d
sythryan <syth.ryan@banno.com>
2014-05-14 09:49:12 -0500

    middle of random testing

---
+++ thirdIteration/milestone/src/main/scala/main.scala
@@ -0,0 +1,26 @@
+// package milestone
+
+// object main {
+
+//   import scala.util.Random
+
+//   def def main(args: Array[String]): Unit = {
+//     ("1", "Joe", 55.34, List(Transaction("tran1", -23.43), Transaction("tran2", 43.00)))
+//     for (i <- 1 to 10) {
+//       val randomUser = User(Random.nextString(4), Random.nextString(8), )
+//       create(randomUser)
+//       for (i < 1 to 100){
+//         val functionToRun = Random(4)
+//         functionToRun match {
+//         case 1 => findUser(Random.nextString(4))
+//         case 2 =>
+//         case 3 =>
+//         case 4 => 
+//       }
+
+//       }
+      
+//     }
+    
+//   }
+// }
