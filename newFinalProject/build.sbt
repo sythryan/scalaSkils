ffda7b0ae1dd3001ca88731182f93eefd37557f7
Syth Ryan <syth.ryan@banno.com>
2014-07-03 11:34:02 -0500

    parsing

--- newFinalProject/build.sbt
+++ newFinalProject/build.sbt
@@ -1,6 +1,25 @@
-libraryDependencies ++= Seq(
-   "net.sourceforge.htmlcleaner" % "htmlcleaner" % "2.2",
-   "org.apache.commons" % "commons-io" % "1.3.2",
-   "org.apache.httpcomponents" % "httpclient" % "4.1.2",
-   "org.mod4j.org.apache.commons" % "lang" % "2.1.0"
-   )
+
+version       := "1.0"
+
+scalaVersion  := "2.10.3"
+
+scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
+
+libraryDependencies ++= {
+  val akkaV = "2.1.4"
+  val sprayV = "1.1.1"
+  Seq(
+    "io.spray"            %   "spray-can"     % sprayV,
+    "io.spray"            %   "spray-routing" % sprayV,
+    "io.spray"            %   "spray-testkit" % sprayV  % "test",
+    "com.typesafe.akka"   %%  "akka-actor"    % akkaV,
+    "com.typesafe.akka"   %%  "akka-testkit"  % akkaV   % "test",
+    "org.specs2"          %%  "specs2"        % "2.2.3" % "test",
+    "net.sourceforge.htmlcleaner" % "htmlcleaner" % "2.2",
+    "org.apache.commons" % "commons-io" % "1.3.2",
+    "org.apache.httpcomponents" % "httpclient" % "4.1.2",
+    "org.mod4j.org.apache.commons" % "lang" % "2.1.0"
+  )
+}
+
+Revolver.settings
--- newFinalProject/src/main/scala/rssFeed.scala
+++ newFinalProject/src/main/scala/rssFeed.scala
@@ -1,32 +1,37 @@
 package main.scala
 
+
 import org.apache.http.impl.client.DefaultHttpClient
-import java.net.{URLConnection, URL}
-import java.util.ArrayList
-import java.util.Scanner
 import org.apache.http.protocol._
 import org.apache.commons.io._
-import org.apache.http.message.BasicNameValuePair
-import org.apache.http.client.entity.UrlEncodedFormEntity
-import org.apache.http.HttpEntity
-import org.apache.http.util.EntityUtils
 import org.apache.http.client.HttpClient
-import org.apache.http.client.methods.{HttpGet, HttpPost}
-import java.io.{BufferedReader, InputStreamReader, InputStream}
-import scala.xml._
-import java.io._
-import scala.Console
+import org.apache.http.client.methods.HttpGet
+import scala.xml.XML
 
 
 object retrieve {
 
-	def main(args: Array[String]): Unit = {
+	private [this] def parseArticles(document: String, feed: Elem): List[Article] = {
+
+		(feed \\ "entry").foreach {parent =>
+			for {
+				published <- (parent \ "published")
+				updated <- (parent \ "updated")
+				title <- (parent \ "title")
+				id <- (parent \ "id")
+				author <- (parent \ "author" \ "name")
+			} yield {
+				Article(id, title, author, published, updated, "not implemented")
+			}
+		}
+	}
+
+	def loadNewXML = {
 		val httpclient: HttpClient   = new DefaultHttpClient();
     val context:    HttpContext  = new BasicHttpContext
     			
 		val httpGetOne = new HttpGet("http://www.theverge.com/rss/frontpage.xml")
-		val response =  XML.loadString(IOUtils.toString((httpclient.execute(httpGetOne, context).getEntity.getContent)))
+		val response = XML.loadString(IOUtils.toString((httpclient.execute(httpGetOne, context).getEntity.getContent)))
 
-		println(response)	
 	} 
 }
